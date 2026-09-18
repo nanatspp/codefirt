be62d62f1b5a17c68dd42fca2510d6aaed6ebd6f
52701512867 <52701512867@D05S22-1252884>
2026-09-18 10:55:33 -0300

    Adicionar arquivos de projeto.

---
+++ codefirt/Data/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using codefirt.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace codefirt.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Desenvolvedora> Desenvolvedoras => Set<Desenvolvedora>();
+        public DbSet<Game> Games => Set<Game>();
+        public DbSet<Genero> Generos => Set<Genero>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Desenvolvedora>()
+                .HasMany(d => d.Games)
+                .WithOne(d => d.Desenvolvedora)
+                .HasForeignKey(d => d.DesenvolvedoraId);
+
+            modelBuilder.Entity<Game>()
+                .HasMany(g => g.Generos)
+                .WithMany(g => g.Games);
+        }
+
+    }
+}
---
+++ codefirt/Program.cs
@@ -0,0 +1,48 @@
+using codefirt.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+
+var app = builder.Build();
+app.MapControllers();
+app.Run();
+
+
+//namespace codefirt
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var builder = WebApplication.CreateBuilder(args);
+
+//            // Add services to the container.
+
+//            builder.Services.AddControllers();
+//            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//            builder.Services.AddOpenApi();
+
+//            var app = builder.Build();
+
+//            // Configure the HTTP request pipeline.
+//            if (app.Environment.IsDevelopment())
+//            {
+//                app.MapOpenApi();
+//            }
+
+//            app.UseHttpsRedirection();
+
+//            app.UseAuthorization();
+
+
+//            app.MapControllers();
+
+//            app.Run();
+//        }
+//    }
+//}
